6c33c2bf937c1ea79417e7f36db6f7470d30f0b9
Saul <saullionlee@hotmail.com>
2021-08-01 17:26:40 -0400

    commit

--- src/Helpers/API.js
+++ src/Helpers/API.js
@@ -1,4 +1,5 @@
 export const API = {
-    //api:`http://localhost:3001`,
-    api:`https://booksystem66.herokuapp.com`
-}
+	//api: `http://localhost:3001`,
+	//api:`https://booksystem66.herokuapp.com`
+	api: `http://149.248.62.77:3001`,
+};
